350db71881b4501ef44cadfa5b1d66154d87c31e
Gadhe-Sai-prasad <154505332+Gadhe-Sai-prasad@users.noreply.github.com>
2025-09-11 17:45:43 +0530

    Add files via upload

---
+++ aarrayList.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class aarrayList {
+    public static void main(String[] args) {
+        ArrayLis2t<String> studentName = new ArrayLis2t<>();
+
+        studentName.add("Rakesh");
+
+        
+    }
+
+}
