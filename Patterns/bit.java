f74c10b35059a4d2bd4e74f69c5bdcc6c905a743
Gadhe-Sai-prasad <154505332+Gadhe-Sai-prasad@users.noreply.github.com>
2025-09-26 17:46:35 +0530

    Add files via upload

---
+++ Patterns/bit.java
@@ -0,0 +1,10 @@
+public class bit {
+    public static void main(String[] args) {
+        int x = 13;
+        int pos = 3;
+        int b = (pos-1);
+        int c = ~((int)Math.pow(2,3));
+        System.out.print(x&c);
+        }
+    
+}
