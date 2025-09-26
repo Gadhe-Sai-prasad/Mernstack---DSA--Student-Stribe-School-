e9497d727e63b71d55616cf0f3c710f318965e0e
Gadhe-Sai-prasad <154505332+Gadhe-Sai-prasad@users.noreply.github.com>
2025-09-26 17:47:36 +0530

    Add files via upload

---
+++ Patterns/Pattern2.java
@@ -0,0 +1,15 @@
+public class Pattern2 {
+    public static void main(String[] args) {
+        int n =3;
+        for(int i =1;i<=n;i++) {
+            int cval = 1;
+            for(int j = 1; j<=n;j++){
+                System.out.print(cval);
+
+            }
+            System.out.println(" ");
+            
+        }
+    }
+    
+}
